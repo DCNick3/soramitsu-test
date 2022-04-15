fd0b7b27b5c715570e8dd11551f3beeb974f7306
Nikita Strygin <nikita6@bk.ru>
2022-04-15 20:33:16 +0300

    Implement skeleton for the pallet: add necessary storage, events and define dispatchable functions

--- pallets/erc20/src/lib.rs
+++ pallets/erc20/src/lib.rs
@@ -18,6 +18,7 @@ mod benchmarking;
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use sp_core::U256;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -42,22 +43,26 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn balance_of)]
+	pub type Balance<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, U256>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn allowance)]
+	pub type Allowance<T: Config> =
+		StorageMap<_, Blake2_128Concat, (T::AccountId, T::AccountId), U256>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn total_supply)]
+	pub type TotalSupply<T> = StorageValue<_, U256>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		Transfer { from: T::AccountId, to: T::AccountId, amount: U256 },
+		Approval { owner: T::AccountId, spender: T::AccountId, amount: U256 },
 	}
 
 	// Errors inform users that something went wrong.
@@ -69,46 +74,36 @@ pub mod pallet {
 		StorageOverflow,
 	}
 
+	// private (non-dispatchable) functions
+	impl<T: Config> Pallet<T> {}
+
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
+		#[pallet::weight(10_000)] // TODO
+		pub fn transfer(origin: OriginFor<T>, to: T::AccountId, amount: U256) -> DispatchResult {
+			todo!()
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(10_000)] // TODO
+		pub fn approve(
+			origin: OriginFor<T>,
+			spender: T::AccountId,
+			amount: U256,
+		) -> DispatchResult {
+			todo!()
+		}
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+		#[pallet::weight(10_000)] // TODO
+		pub fn transfer_from(
+			origin: OriginFor<T>,
+			from: T::AccountId,
+			to: T::AccountId,
+			amount: U256,
+		) -> DispatchResult {
+			todo!()
 		}
 	}
 }
